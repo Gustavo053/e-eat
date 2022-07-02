e509b683f693cfeb5a624f2e0823bee93d9a2459
gustavo <gugupf10@gmail.com>
2022-07-02 14:32:38 -0300

    Remove imports

--- api-gateway/src/main/java/com/eeat/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/eeat/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,6 @@ package com.eeat.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
